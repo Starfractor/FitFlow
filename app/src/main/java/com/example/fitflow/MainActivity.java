e9ad90913155d5270a9f52adedba0e17a0128ae2
James Pantigoso <jpantigo@uci.edu>
2024-02-08 22:49:19 -0800

    made home page and connected to login

--- app/src/main/java/com/example/fitflow/MainActivity.java
+++ app/src/main/java/com/example/fitflow/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.fitflow;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Assume your login button has an ID of btnLogin in the XML layout
+        Button loginButton = findViewById(R.id.btnLogin);
+
+        // Set an OnClickListener for the login button
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // When the button is clicked, navigate to the HomePageActivity
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 
