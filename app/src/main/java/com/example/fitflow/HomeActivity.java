380be72e4236f37c6f1ee0ca37de7dc7b8521746
Nicholas King <starfractor@gmail.com>
2024-02-15 12:14:38 -0800

    Added home button functionality

--- app/src/main/java/com/example/fitflow/HomeActivity.java
+++ app/src/main/java/com/example/fitflow/HomeActivity.java
@@ -1,8 +1,10 @@
 package com.example.fitflow;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        Button buttonLogout = findViewById(R.id.buttonLogout);
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Perform action to logout user
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
