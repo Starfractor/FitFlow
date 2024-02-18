7a33bd428c261f3c93df266ff77728f311936307
ksh35 <krishmeghani01@gmail.com>
2024-02-18 01:37:01 -0800

    Location related permission stuff

---
+++ app/src/main/java/com/example/fitflow/FoodRecActivity.java
@@ -0,0 +1,20 @@
+package com.example.fitflow;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+public class FoodRecActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+    private void getLocation(){
+
+    }
+    private void requestLocationPermissions(){
+
+    }
+}
